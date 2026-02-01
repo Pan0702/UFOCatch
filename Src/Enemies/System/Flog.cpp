4ee7be12aa1af24969b44a6393f5053053052fa8
Pan0702 <candywanwan1996@gmail.com>
2026-02-01 18:22:57 +0900

    群れの情報計算ロジックを追加し、羊管理の機能を拡張

--- Src/Enemies/System/Flog.cpp
+++ Src/Enemies/System/Flog.cpp
@@ -1,1 +1,32 @@
 #include "Flog.h"
+
+void Flog::RemoveFromArray(CSheep* sheep)
+{
+    sheeps.erase(std::remove(sheeps.begin(), sheeps.end(), sheep), sheeps.end());
+}
+
+FlogInfo Flog::CalcFlogInfo() const
+{
+    FlogInfo info;
+    info.centroid = VECTOR3(0,0,0);
+    info.maxDistance = 0.0f;
+    info.furthestSheep = nullptr;
+    
+    if (sheeps.empty())return info;
+    for (auto sheep : sheeps)   
+    {  
+        info.centroid += sheep->GetTransform().position;
+    }
+    info.centroid /= sheeps.size();
+    
+    for (auto sheep : sheeps)
+    {
+        float distance = (info.centroid - sheep->GetTransform().position).Length();
+        if (distance > info.maxDistance)
+        {
+            info.maxDistance = distance;
+            info.furthestSheep = sheep;
+        }
+    }
+    return info;
+}
--- Src/Enemies/System/Flog.h
+++ Src/Enemies/System/Flog.h
@@ -3,12 +3,11 @@
 #include "../AnimalSheep/Sheep.h"
 
 //群れに関する情報
-//centroid 群れの重心
 struct FlogInfo
 {
-    VECTOR3 centroid; //群れの重心
-    CSheep* furthestSheep;
-    float maxDistance;
+    VECTOR3 centroid;       //群れの重心
+    CSheep* furthestSheep;  //一番遠くにいる羊のptr;
+    float maxDistance;      //中心から一番遠くにいる動物までの距離
 };
 class Flog : public Object3D
 {
@@ -20,5 +19,8 @@ public:
 private:
 
     void Spawn(int a);
-    void RemoveFromArray();
+    void RemoveFromArray(CSheep* sheep);
+    FlogInfo CalcFlogInfo() const;
+    FlogInfo CalcFlogInfo();
+    std::vector<CSheep*> sheeps;
 };
